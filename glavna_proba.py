274040c855fd98b5f76bff265747c774556dca96
sabldabl <93114921+sabldabl@users.noreply.github.com>
2023-06-08 12:50:31 +0200

    glavna_proba.py

--- glavna_proba.py
+++ glavna_proba.py
@@ -25,7 +25,7 @@ def get_os():
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
